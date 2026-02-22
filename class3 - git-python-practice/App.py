e3fd66ad880bb143944121de536ebd3cf9ad668d
mangisto14 <mangisto1410@gmail.com>
2026-02-23 01:31:56 +0200

    Fix: adding values-dev.yaml and charts

--- class3 - git-python-practice/App.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def greet(name):
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"👋 Hello, {name}! ⏰ Current time: {current_time}"
-
-if __name__ == "__main__":
-    user = "World"
-    print(greet(user))
